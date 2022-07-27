0e50ec7a9875d26872fde1b1da69a3a69d07043e
mkarampatsis <marka@central.ntua.gr>
2022-07-27 21:14:07 +0300

    comment .env

--- model/OpenApiSpec.php
+++ model/OpenApiSpec.php
@@ -45,7 +45,7 @@ use OpenApi\Annotations as OA;
 *     description="Anouncements API",
 * )
 * @OA\Server(
-*     url="http://localhost/",
+*     url="http://coding-factory-test.herokuapp.com/",
 *     description="API server"
 * )
 * @OA\Components(
